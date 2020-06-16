ffbd1e896399220069e092d15f42a87abedf47b3
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-06-16 15:35:51 +0500

    Updates

---
+++ precache-manifest.2f9fbf6b7fb31d573fdac046642f5532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02e3037d2c7234ee056d13cf7c44922",
+    "url": "/react-smart-brain/index.html"
+  },
+  {
+    "revision": "517a59d0ec07e129b80a",
+    "url": "/react-smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d3928c3576995d2fdf7d",
+    "url": "/react-smart-brain/static/css/main.fbfa3b87.chunk.css"
+  },
+  {
+    "revision": "517a59d0ec07e129b80a",
+    "url": "/react-smart-brain/static/js/2.1cb3676d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-smart-brain/static/js/2.1cb3676d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3928c3576995d2fdf7d",
+    "url": "/react-smart-brain/static/js/main.ed72eedf.chunk.js"
+  },
+  {
+    "revision": "a4fee3b3cafa7c43bfa4",
+    "url": "/react-smart-brain/static/js/runtime-main.92385cce.js"
+  }
+]);
